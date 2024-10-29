5888e8e6681216904015c9346afe46f0fabb88c8
Cutleast <cutleast@gmail.com>
2024-10-29 21:52:39 +0100

    Add versioninfo and merge pyinstaller commands with spec file

--- DIP.spec
+++ DIP.spec
@@ -1,7 +1,6 @@
 # -*- mode: python ; coding: utf-8 -*-
 
-
-a = Analysis(
+gui_a = Analysis(
     ['src\\main.py'],
     pathex=[],
     binaries=[],
@@ -14,11 +13,10 @@ a = Analysis(
     noarchive=False,
     optimize=0,
 )
-pyz = PYZ(a.pure)
-
-exe = EXE(
-    pyz,
-    a.scripts,
+gui_pyz = PYZ(gui_a.pure)
+gui_exe = EXE(
+    gui_pyz,
+    gui_a.scripts,
     [],
     exclude_binaries=True,
     name='DIP',
@@ -32,12 +30,51 @@ exe = EXE(
     target_arch=None,
     codesign_identity=None,
     entitlements_file=None,
-    icon=['res\\icons\\icon.ico'],
+    version='version.txt',
+    icon=['res\\icons\\icon.ico']
 )
+
+cli_a = Analysis(
+    ['src\\main.py'],
+    pathex=[],
+    binaries=[],
+    datas=[],
+    hiddenimports=[],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    noarchive=False,
+    optimize=0,
+)
+cli_pyz = PYZ(cli_a.pure)
+cli_exe = EXE(
+    cli_pyz,
+    cli_a.scripts,
+    [],
+    exclude_binaries=True,
+    name='DIP_cli',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    console=True,
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+    version='version.txt',
+    icon=['res\\icons\\icon.ico']
+)
+
 coll = COLLECT(
-    exe,
-    a.binaries,
-    a.datas,
+    gui_exe,
+    gui_a.binaries,
+    gui_a.datas,
+    cli_exe,
+    cli_a.binaries,
+    cli_a.datas,
     strip=False,
     upx=True,
     upx_exclude=[],
--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
+"""
+Copyright (c) Cutleast
+"""
+
 import os
 import shutil
 from pathlib import Path
+
+import pyinstaller_versionfile
 
 # Application details
 APPNAME = "Dynamic Interface Patcher"
@@ -11,12 +17,12 @@ DIST_FOLDER = Path("dist").resolve()
 BUILD_FOLDER = Path("build").resolve()
 FOMOD_FOLDER = Path("fomod").resolve()
 OUTPUT_FOLDER = Path("DIP_with_fomod").resolve() / "fomod"
-OBSOLETE_ITEMS: list[Path] = [DIST_FOLDER / "qtpy" / "tests"]
+OBSOLETE_ITEMS: list[Path] = []
 ADDITIONAL_ITEMS: dict[Path, Path] = {
-    Path("res") / "7-zip": DIST_FOLDER / "7-zip",
-    Path("res") / "ffdec": DIST_FOLDER / "ffdec",
-    Path("res") / "jre.7z": DIST_FOLDER / "jre.7z",
-    Path("res") / "xdelta": DIST_FOLDER / "xdelta",
+    Path("res") / "7-zip": OUTPUT_FOLDER / "DIP" / "7-zip",
+    Path("res") / "ffdec": OUTPUT_FOLDER / "DIP" / "ffdec",
+    Path("res") / "jre.7z": OUTPUT_FOLDER / "DIP" / "jre.7z",
+    Path("res") / "xdelta": OUTPUT_FOLDER / "DIP" / "xdelta",
 }
 
 for folder in [DIST_FOLDER, BUILD_FOLDER]:
@@ -24,17 +30,28 @@ for folder in [DIST_FOLDER, BUILD_FOLDER]:
         shutil.rmtree(folder)
         print(f"Deleted {folder} folder.")
 
-print("Creating Executables with PyInstaller...")
-os.system(
-    f'pyinstaller --noconfirm --windowed --name DIP '
-    f'--icon=./res/icons/icon.ico '
-    f'--distpath {DIST_FOLDER} --workpath {BUILD_FOLDER} ./src/main.py'
+print("Creating version.txt...")
+pyinstaller_versionfile.create_versionfile_from_input_file(
+    output_file="version.txt",
+    input_file="version.yml",
+    version=VERSION,
 )
+
+print("Building Executables with PyInstaller...")
 os.system(
-    f'pyinstaller --noconfirm --console --name DIP_cli '
-    f'--icon=./res/icons/icon.ico '
-    f'--distpath {DIST_FOLDER} --workpath {BUILD_FOLDER} ./src/main.py'
+    f"pyinstaller " "--noconfirm " "--clean " f"--distpath {DIST_FOLDER} " "DIP.spec"
 )
+
+print("Packing with FOMOD...")
+if OUTPUT_FOLDER.is_dir():
+    shutil.rmtree(OUTPUT_FOLDER)
+    print("Deleted already existing output folder.")
+
+print("Copying FOMOD...")
+shutil.copytree(FOMOD_FOLDER, OUTPUT_FOLDER, dirs_exist_ok=True)
+
+print("Copying DIP...")
+shutil.copytree(DIST_FOLDER / "DIP", OUTPUT_FOLDER / "DIP", dirs_exist_ok=True)
 
 print(f"Copying {len(ADDITIONAL_ITEMS)} additional item(s)...")
 for item, dest in ADDITIONAL_ITEMS.items():
@@ -46,25 +63,14 @@ for item, dest in ADDITIONAL_ITEMS.items():
     else:
         print(f"{str(item)!r} does not exist!")
         continue
-    print(f"Copied {str(item)!r} to {str(dest.relative_to(DIST_FOLDER))!r}.")
+    print(f"Copied {str(item)!r} to {str(dest.relative_to(OUTPUT_FOLDER))!r}.")
 
 for item in OBSOLETE_ITEMS:
     if item.is_file():
         os.remove(item)
     elif item.is_dir():
         shutil.rmtree(item)
-    print(f"Removed item {str(item.relative_to(DIST_FOLDER))!r} from dist folder.")
-
-print("Packing with FOMOD...")
-if OUTPUT_FOLDER.is_dir():
-    shutil.rmtree(OUTPUT_FOLDER)
-    print("Deleted already existing output folder.")
-
-print("Copying FOMOD...")
-shutil.copytree(FOMOD_FOLDER, OUTPUT_FOLDER, dirs_exist_ok=True)
-
-print("Copying DIP...")
-shutil.copytree(DIST_FOLDER, OUTPUT_FOLDER / "DIP", dirs_exist_ok=True)
+    print(f"Removed item {str(item.relative_to(OUTPUT_FOLDER))!r} from dist folder.")
 
 # Update version in info.xml
 info_xml_path = OUTPUT_FOLDER / "info.xml"
@@ -77,27 +83,6 @@ if info_xml_path.is_file():
                 line = f"\t<Version>{VERSION}</Version>\n"
             file.write(line)
     print(f"Updated version in {info_xml_path} to {VERSION}.")
-
-# Move content of DIP/DIP_cli and DIP/DIP to DIP and handle name conflicts
-dip_folder = OUTPUT_FOLDER / "DIP"
-dip_subfolders = [dip_folder / "DIP_cli", dip_folder / "DIP"]
-
-print("Combining DIP and DIP_cli into one folder...")
-for subfolder in dip_subfolders:
-    if subfolder.is_dir():
-        for item in subfolder.iterdir():
-            dest = dip_folder / item.name
-            if dest.exists():
-                # If a conflict occurs, overwrite the existing file or merge directories
-                if dest.is_file():
-                    dest.unlink()  # Delete existing file to avoid conflict
-                elif dest.is_dir():
-                    shutil.rmtree(dest)  # Delete existing directory to avoid conflict
-            shutil.move(str(item), dip_folder)
-        # Check if subfolder is empty before removing
-        if not any(subfolder.iterdir()):
-            subfolder.rmdir()
-            print(f"Deleted empty folder {subfolder}.")
 
 print("Packing into 7-zip archive...")
 if Path(f"DIP_v{VERSION}.7z").is_file():
